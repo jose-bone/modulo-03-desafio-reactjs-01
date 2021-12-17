d9512394e5fa31a7f74fd21af746460418609664
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-17 17:08:16 +0200

    Formatação de date-fns

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function formatDate(date: string) {
+  return format(new Date(date), 'dd MMM yyyy', {
+    locale: ptBR,
+  });
+}
