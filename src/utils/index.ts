613b38a40db2fa978d84bdfadcccd197899b9dc6
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-17 20:41:03 +0200

    Formatação da Data

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,8 +3,12 @@ import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function formatDate(date: string) {
-  return format(new Date(date), 'dd MMM yyyy', {
-    locale: ptBR,
-  });
+export function formatDate(date: string, withTime = false) {
+  return format(
+    new Date(date),
+    withTime ? "dd MMM yyyy' às 'HH:mm'" : 'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
 }
