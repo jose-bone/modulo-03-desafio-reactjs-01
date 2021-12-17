bc6c8dd870665cf47b3c293e2961726c82995cbb
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-17 18:44:22 +0200

    Atualização do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Link from 'next/link';
 
 import styles from './header.module.scss';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function Header() {
   return (
     <header className={styles.header}>
